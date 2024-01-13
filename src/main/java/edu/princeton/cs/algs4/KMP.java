786d159871ce17c5c5fbafd537d59a25d6160eae
Daniel Senften <daniel.senften@talent-factory.ch>
2024-01-13 12:29:31 +0100

    Update copyright notices in KMP.java
    The commit moves the copyright notices in the KMP.java file to the top, removes duplicate entries, and adjusts the wording for clarity. It contributes to aligning the file structure across the project and ensuring proper copyright attribution.

--- src/main/java/edu/princeton/cs/algs4/KMP.java
+++ src/main/java/edu/princeton/cs/algs4/KMP.java
@@ -1,3 +1,26 @@
+/*
+ * Copyright 2002-2020, Robert Sedgewick and Kevin Wayne
+ *
+ *  This file is part of algs4, which accompanies the textbook
+ *
+ *      Algorithms, 4th edition by Robert Sedgewick and Kevin Wayne,
+ *      Addison-Wesley Professional, 2011, ISBN 0-321-57351-X.
+ *      http://algs4.cs.princeton.edu/
+ *
+ *  algs4 is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version
+ *
+ *  algs4 is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with algs4.  If not, see http://www.gnu.org/licenses.
+ */
+
 /******************************************************************************
  *  Compilation:  javac KMP.java
  *  Execution:    java KMP pattern text
@@ -166,27 +189,3 @@ public class KMP {
         StdOut.println(pat);
     }
 }
-
-/*
- *  Copyright 2002-2020, Robert Sedgewick and Kevin Wayne.
- *
- *  This file is part of algs4.jar, which accompanies the textbook
- *
- *      Algorithms, 4th edition by Robert Sedgewick and Kevin Wayne,
- *      Addison-Wesley Professional, 2011, ISBN 0-321-57351-X.
- *      http://algs4.cs.princeton.edu
- *
- *
- *  algs4.jar is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  algs4.jar is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with algs4.jar.  If not, see http://www.gnu.org/licenses.
- ******************************************************************************/
